403054834cec2fedafa8cabc5f9493aa0550d8a9
sojys <sojys@naver.com>
2024-03-11 11:28:01 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    // main 입력하고 탭
+    public static void main(String[] args) {
+        // sysout 이 아니라 , sout 입니다.
+        // sout 탭
+        System.out.println("Hi");
+    }
+}
